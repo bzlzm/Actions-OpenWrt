3965276e4fe1982d5ec8c59182f9f80e25ff4467
bzlzm <46715173+bzlzm@users.noreply.github.com>
2024-04-03 20:09:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
-echo 'src-git helloworld https://github.com/solana-labs/example-helloworld.git' >>feeds.conf.default
+echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >>feeds.conf.default
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/solana-labs/example-helloworld.git' >>feeds.conf.default
